6a5016099c6775109438ac2a6791b479dd5a2b62
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-19 13:40:18 +0530

    add next/auth

---
+++ auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { authConfig } from './auth.config';
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+      },
+    }),
+  ],
+});
